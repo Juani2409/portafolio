304eb9c5c4a596252def656322b18bc4e3acea83
Juan Ignacio Grodz <jigrodz@gmail.com>
2023-08-02 18:07:52 -0300

    modificando el carousel

--- src/componentes/cards.jsx
+++ src/componentes/cards.jsx
@@ -4,54 +4,40 @@ function Card() {
   return (
 
     <div class="bg-dark container-fluid py-5" >
-     
+      <section id="trabajos">
         <h1 style={{
         display: "flex",
         alignItems: "center",
         justifyContent: "center"
       }}>Mis Trabajos</h1>
-       <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
+       
+
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Etiqueta de la primera diapositiva</h5>
-        <p>Algún contenido placeholder representativo para la primera diapositiva.</p>
-      </div>
+    <img src="../logos/pi.jpg" class="d-block w-100" alt="Imagen Pi" />
     </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Etiqueta de la segunda diapositiva</h5>
-        <p>Algún contenido placeholder representativo para la segunda diapositiva.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="../logos/HTML-CSS.png" class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Etiqueta de la tercera diapositiva</h5>
-        <p>Algún contenido placeholder representativo para la tercera diapositiva.</p>
-      </div>
-    </div>
+   
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Anterior</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Siguiente</span>
   </button>
 </div>
-      
+
+
+
+         
+        
+      </section>
     </div>
   )
 
 }
 
 export default Card;
+
